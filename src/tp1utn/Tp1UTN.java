fc754911705d2b6df0e8578e0ee8ab99d4176d48
AnaGarcia58 <141678216+AnaGarcia58@users.noreply.github.com>
2023-11-30 15:24:00 -0300

    Update Tp1UTN.java
    Modificación de la ejecución del código en el Main

--- src/tp1utn/Tp1UTN.java
+++ src/tp1utn/Tp1UTN.java
@@ -1,56 +1,11 @@
 package tp1utn;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import tp1utn.Inscripcion;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+
 import static tp1utn.Inscripcion.verificarInscripcion;
 
 public class Tp1UTN {
 
     public static void main(String[] args) {
-//
-//        Alumno alu = new Alumno("Mark", "Sloan");
-//        Alumno alu1 = new Alumno("Meredith", "Grey");
-//        Alumno alu2 = new Alumno("Cristina", "Yang");
-//        Alumno alu3 = new Alumno("Owen", "Hunt");
-//        Alumno alu4 = new Alumno("Amelia", "Sheperd");
-//        Alumno alu5 = new Alumno("Richard", "Webber");
-//
-//        List<Alumno> alumnos = Arrays.asList(alu, alu1, alu2, alu3, alu4, alu5);
-//
-////        System.out.println(alumnos);
-//
-//        Materia algoritmos = new Materia("Algoritmos y Estructuras de Datos");
-//        Materia paradigmas = new Materia("Paradigmas de Programación");
-//        Materia diseñoSistemas = new Materia("Diseño de Sistemas");
-//
-//        // Establecer correlatividades
-//        algoritmos.agregarCorrelativa(paradigmas);
-//        paradigmas.agregarCorrelativa(diseñoSistemas);
-//
-//        // Crear inscripciones para el alumno
-//        Inscripcion inscripcionAlgoritmos = new Inscripcion(alu1, algoritmos, true);
-//        Inscripcion inscripcionParadigmas = new Inscripcion(alu1, paradigmas, false); // Si pongo false acá igual me la toma aprobada en el if
-//        Inscripcion inscripcionDiseñoSistemas = new Inscripcion(alu1, diseñoSistemas, false);// Si pongo false acá recién ahí no me daja inscribir
-//
-//        
-//// Agregar inscripciones al alumno utilizando la lista de inscripciones del alumno
-//        alu1.getInscripciones().add(inscripcionAlgoritmos);
-//        alu1.getInscripciones().add(inscripcionParadigmas);
-//        alu1.getInscripciones().add(inscripcionDiseñoSistemas);
-//
-//        // Verificar si el alumno puede inscribirse a Diseño de Sistemas
-//        
-//        if (inscripcionParadigmas.aprobada()) {
-//            System.out.println("El alumno "+alu1.getNombre() +", "+alu1.getApellido() +" puede inscribirse a Diseño de Sistemas.");
-//        } else {
-//            System.out.println("EL alumno no cumple con las correlativas necesarias para Diseño de Sistemas.");
-//        }
-//    }
-
 
 
         
